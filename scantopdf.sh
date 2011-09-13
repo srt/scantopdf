341c320213e8824963d2c50b6793b10fc568d2fc
Stefan Reuter <stefan.reuter@reucon.com>
2011-09-14 00:35:28 +0200

    first commit

---
+++ scantopdf.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 options [file.pdf]
+
+This script scans a document and produces a PDF file.
+
+OPTIONS:
+   -h      Show this message
+   -d      Duplex
+   -m      Mode, e.g. Lineart or Gray
+   -r      Resolution in DPI
+   -s      Page number to start naming files with
+   -t      Title of PDF file
+EOF
+}
+
+SOURCE="ADF Front"
+MODE="Lineart"
+RESOLUTION="150"
+BATCH_START="1"
+TITLE=`uuidgen`
+SUBJECT="${TITLE}"
+
+while getopts "hdm:r:s:t:" OPTION
+do
+	case $OPTION in
+		h) usage; exit 1 ;;
+		d) SOURCE="ADF Duplex" ;;
+		m) MODE=$OPTARG ;;
+		r) RESOLUTION=$OPTARG ;;
+		s) BATCH_START=$OPTARG ;;
+		t) TITLE="$OPTARG"; SUBJECT="$OPTARG" ;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+DEST_DIR="."
+unset DEST_FILE
+
+if [ $# == 1 ]
+then
+	DEST_FILE="$1"
+	if [ -e "$DEST_FILE" ]
+	then
+		echo Error: $1 already exists
+		exit 1
+	fi
+	DEST_DIR=$(mktemp -td scantopdf.XXXXXXXXX) || exit 1
+fi
+
+scanimage \
+  --batch="${DEST_DIR}/out%03d.pnm" \
+  --batch-start=${BATCH_START} \
+  --resolution=${RESOLUTION} \
+  -l 0 -t 0 -x 210 -y 297 \
+  --page-width 210 --page-height 297 \
+  --rollerdeskew=yes \
+  --swcrop=yes \
+  --stapledetect=yes \
+  --source "${SOURCE}"
+
+#  --df-thickness=yes \
+#unpaper -v --dpi ${RESOLUTION} -s a4 "${DEST_DIR}/out%03d.pnm" "${DEST_DIR}/unpaper_out%03d.pnm"
+#for i in "${DEST_DIR}/unpaper_out"*; do pnmtotiff "${i}" > "${i}.tif"; done
+
+for i in "${DEST_DIR}/out"*.pnm
+do
+  pnmtotiff \
+    -xresolution "${RESOLUTION}" \
+    -yresolution "${RESOLUTION}" \
+    -lzw "${i}" > ${DEST_DIR}/`basename "${i}" .pnm`.tif
+  rm "${i}"
+done
+
+if [ ! -z "$DEST_FILE" ]
+then
+	tiffcp "${DEST_DIR}/"*".tif" "${DEST_DIR}/all.tif"
+	abbyyocr9 \
+	  --progressInformation \
+	  -id \
+	  --convertToBWImage \
+	  --recognitionLanguage German \
+	  --inputFileName "${DEST_DIR}/all.tif" \
+	  --outputFileFormat PDFA \
+	  --pdfaExportMode ImageOnText \
+	  --pdfaReleasePageSizeByLayoutSize \
+	  --outputFileName "${DEST_DIR}/result.pdf"
+#	tiff2pdf \
+#	  -j -q 50 \
+#	  -pA4 -x "${RESOLUTION}" -y "${RESOLUTION}" \
+#	  -f \
+#	  -c "reuter network consulting" \
+#	  -a "reuter network consulting" \
+#	  -t "${TITLE}" \
+#	  -s "${SUBJECT}" \
+#	  -o "${DEST_DIR}/result.pdf" "${DEST_DIR}/all.tif"
+	mv "${DEST_DIR}/result.pdf" "$DEST_FILE"
+
+	echo rm -rf "${DEST_DIR}"
+fi
+
